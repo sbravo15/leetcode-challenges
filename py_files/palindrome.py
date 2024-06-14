d59aee5028337c25ecbe7dfbc11481c2036705eb
santi <santibrad2215@hotmail.com>
2024-06-13 21:09:01 -0600

    Changed File Name and Added a description

--- py_files/palindrome.py
+++ py_files/palindrome.py
@@ -1,3 +1,6 @@
+# LeetCode Palindrome Problem Solution 
+# Date: 6/13/2024
+# Given a number, determine if it is a palindrome. A palindrome number reads the same forwards and backwards.
 
 def is_palindrome_number(num):
     num_str = str(num) # from Int to Str
